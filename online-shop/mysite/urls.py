fbce59fa250ef90ab9c8989835f9543056123da6
Виктор Бовыкин <aimsscs@gmail.com>
2025-03-20 20:54:32 +0300

    add apps urls

--- online-shop/mysite/urls.py
+++ online-shop/mysite/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('shop/', include('shopapp.urls')),
+    # path('shop/', include('shopapp.urls')),
     path("", include("frontend.urls")),
+    path("api/", include("authapp.urls")),
 ]
