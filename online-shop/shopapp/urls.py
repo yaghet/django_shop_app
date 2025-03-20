36d4b3238f76771f04fcd9d744689df01a5f18e5
Виктор Бовыкин <aimsscs@gmail.com>
2025-03-20 20:54:53 +0300

    some changes

--- online-shop/shopapp/urls.py
+++ online-shop/shopapp/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
-
-app_name = 'shopapp'
-
-urlpatterns = [
-
-]
+# from django.urls import path
+#
+# from shopapp.views import ProductDetailView
+#
+# app_name = "shopapp"
+#
+# urlpatterns = [
+#     path("product/{id}/", ProductDetailView.as_view(), name="product-detail"),
+# ]
--- online-shop/shopapp/views.py
+++ online-shop/shopapp/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
+# from django.views.generic import DetailView
+#
+# # from shopapp.models import Product
+#
+#
+# class ProductDetailView(DetailView):
+#     # model = Product
