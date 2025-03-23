b2de321626eb54cc97f47a3425f9e154be59603a
Виктор Бовыкин <aimsscs@gmail.com>
2025-03-23 19:55:22 +0300

    add media root

--- online-shop/mysite/urls.py
+++ online-shop/mysite/urls.py
@@ -17,10 +17,20 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path('shop/', include('shopapp.urls')),
     path("", include("frontend.urls")),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
+
     path("api/", include("authapp.urls")),
-]
+    path("api/", include("profileapp.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
