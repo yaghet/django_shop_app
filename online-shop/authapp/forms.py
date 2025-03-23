829b530d6ca8eec06f29d447ac23448f4cfca851
Виктор Бовыкин <aimsscs@gmail.com>
2025-03-23 19:54:25 +0300

    del forms add serializer

--- online-shop/authapp/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class RegisterForm(forms.Form):
-    username = forms.CharField(min_length=5, max_length=30)
-    password = forms.CharField(widget=forms.PasswordInput)
