69b4fff7dc7daeaed0b234f05426664f94189ce2
Виктор Бовыкин <aimsscs@gmail.com>
2025-03-23 19:56:18 +0300

    Refactor a few View's

--- online-shop/authapp/views.py
+++ online-shop/authapp/views.py
@@ -1,61 +1,80 @@
-import json
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.exceptions import ValidationError
 from rest_framework.request import Request
-from authapp.auth_services import (authenticate_and_login, register_and_login, logout_user)
+from rest_framework.response import Response
+from rest_framework.status import (HTTP_200_OK, HTTP_400_BAD_REQUEST,
+                                   HTTP_401_UNAUTHORIZED)
+from rest_framework.views import APIView
 
-from authapp.forms import RegisterForm
+from authapp.auth_services import (authenticate_and_login, get_data_from_body,
+                                   load_json, logout_user, register_and_login)
+from authapp.serializers import (LogoutSerializer, UserLoginSerializer,
+                                 UserRegistrationSerializer)
 
 
-class LoginView(APIView):
+class BaseAuthView(APIView):
 
-    """View представления для входа в приложение"""
+    """ Базовая форма для регистрации и аутентификации в API """
+
+    _status_response_200 = HTTP_200_OK
+    _status_response_400 = HTTP_400_BAD_REQUEST
+    _status_response_401 = HTTP_401_UNAUTHORIZED
+
+    def load_json(self, request: Request):
+        try:
+            return load_json(body=request.body)
+        except ValueError as exp:
+            return Response({"Error": str(exp)}, status=self._status_response_400)
+
+
+class LoginView(BaseAuthView):
+
+    """ View представления для аутентификации пользователя (войти в приложение) """
+
+    serializer_class = UserLoginSerializer
 
     def post(self, request: Request):
 
-        body = json.loads(request.body)
+        user_data = self.load_json(request)
+        serializer = self.serializer_class(data=user_data)
+        if serializer.is_valid():
+            username, password = get_data_from_body(user_data)
 
-        username = body['username']
-        password = body['password']
+            if authenticate_and_login(request, username, password):
+                return Response(status=self._status_response_200)
 
-        if authenticate_and_login(request, username, password):
-            return Response(status=status.HTTP_200_OK)
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+            return Response(status=self._status_response_401)
+        else:
+            print(serializer.errors)
+            return Response({"Error": serializer.errors}, status=self._status_response_401)
 
 
-class RegisterView(APIView):
+class RegisterView(BaseAuthView):
 
     """ View представление для регистрации пользователя """
 
-    form_class = RegisterForm
+    serializer_class = UserRegistrationSerializer
 
     def post(self, request: Request):
-        try:
-            user_data = json.loads(request.body)
-        except json.JSONDecodeError:
-            return Response({"Error": "Invalid JSON"}, status=status.HTTP_400_BAD_REQUEST)
 
-        form = self.form_class(data=user_data)
+        user_data = self.load_json(request)
+        serializer = self.serializer_class(data=user_data)
 
-        if form.is_valid():
+        if serializer.is_valid():
+            username, password, full_name = get_data_from_body(body=user_data)
 
-            username = user_data.get("username")
-            password = user_data.get("password")
-
-            if register_and_login(request, username, password):
-                return Response(status=status.HTTP_200_OK)
+            if register_and_login(request, username, password, full_name):
+                return Response(status=self._status_response_200)
             else:
-                return Response({"Error": "Registration failed"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"Error": "Registration failed"}, status=self._status_response_401)
         else:
-            return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=self._status_response_401)
 
 
 class LogoutView(APIView):
 
-    """View представление для выхода пользователя из приложения"""
+    """ View представление для выхода пользователя из приложения """
+
+    serializer_class = LogoutSerializer
 
     def post(self, request: Request):
         logout_user(request)
-        return Response(status=status.HTTP_200_OK)
+        return Response(status=HTTP_200_OK)
