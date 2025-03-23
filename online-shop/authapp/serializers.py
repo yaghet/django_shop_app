b4f8e363b47f2222f223b627c799d4b12bfd403e
Виктор Бовыкин <aimsscs@gmail.com>
2025-03-23 19:56:30 +0300

    Init serializers

---
+++ online-shop/authapp/serializers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserLoginSerializer(serializers.Serializer):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'password',
+        )
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'password',
+        )
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+class LogoutSerializer(serializers.Serializer):
+    pass
